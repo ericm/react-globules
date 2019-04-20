ec1a3cc82838b537f4a2c3443d16b4baf30511ee
Eric Moynihan <git@moynihan.io>
2019-04-20 18:53:11 +0100

    Started canvas work

--- demo/src/index.js
+++ demo/src/index.js
@@ -6,10 +6,14 @@ import Example from '../../src'
 class Demo extends Component {
     render() {
         return <div>
-            <Example widthPercent={true} width={100} height={100} />
+            <Example 
+                widthPercent={true} 
+                heightPercent={true} 
+                width={100} 
+                height={100} 
+            />
         </div>
     }
 }
-
-render(<Demo/>, document.querySelector('#demo'))
 render(<style>{"* { margin: 0; padding: 0; border: 0; }"}</style>, document.head)
+render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,33 +1,62 @@
-import React, {Component} from 'react'
-
+import React, {Component} from 'react';
 export default class extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {width: 0}
-        
-        this.ctx = null;
-        document.onresize = this.resize;
+        this.state = {
+            width: 0, 
+            height: 0, 
+            primary: '#80ffb0', 
+            secondary: '#1ac75c', 
+            background: '#3ce47c'
+        };
+        document.body.onresize = this.resize.bind(this);
+        if (!!this.props.background) this.setState({background: this.props.background});
     }
 
     componentDidMount() {
-        if (this.props.widthPercent) {
-            this.resize();
-        }
+        this.resize();
     }
 
-    resize = () => {
-        const width = document.body.offsetWidth*(this.props.width/100);
-        this.setState({width});
+    draw = () => {
+        this.drawBack();
+        this.genRands();
+    }
+
+    genRands = () => {
+        let out = [];
+        for (let _i = 0; _i < this.state.width / 100; _i++) {
+            out.push([Math.random()*(this.state.width-100), Math.random()*(this.state.height-100)]);
+        }
+        console.log(out);
+    }
+
+    drawBack = () => {
+        this.ctx.fillStyle = this.state.background
+        this.ctx.fillRect(0, 0, this.state.width, this.state.height);
+    }
+
+    clear = () => this.ctx.clearRect(0, 0, this.state.width, this.state.height)
+    
+
+    async resize() {
+        if (this.props.heightPercent) {
+            const height = window.innerHeight*(this.props.height/100);
+            await this.setState({height});
+        }
+        if (this.props.widthPercent) {
+            const width = document.body.clientWidth*(this.props.width/100);
+            await this.setState({width});
+        }
+        await this.clear();
+        this.draw();
     }
 
     render() {
-        return <div>
-            <canvas 
+        return <canvas 
                 ref={ref => { if (!!ref) this.ctx = ref.getContext('2d'); }} 
                 width={this.state.width}
-                height={this.props.height} 
+                height={this.state.height} 
             />
-        </div>
     }
 }
